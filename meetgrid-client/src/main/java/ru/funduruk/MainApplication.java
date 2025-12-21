11cee4ae8ba28494e7d795e9b59ca86fb3294571
funduruk <dmitreydima000@gmail.com>
2025-12-21 09:06:17 +0300

    add auth client

---
+++ meetgrid-client/src/main/java/ru/funduruk/MainApplication.java
@@ -0,0 +1,68 @@
+package ru.funduruk;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.util.Objects;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/LoginView.fxml"));
+        BorderPane rootPane = loader.load();
+
+        Scene scene = new Scene(rootPane, 900, 600);
+        scene.getStylesheets().add(
+                Objects.requireNonNull(
+                        getClass().getResource("/css/style.css")
+                ).toExternalForm()
+        );
+
+
+        Image bg = new Image(
+                Objects.requireNonNull(
+                        getClass().getResource("/image/background/login_background.jpg")
+                ).toExternalForm()
+        );
+
+        ImageView bgView = new ImageView(bg);
+
+        bgView.fitWidthProperty().bind(rootPane.widthProperty());
+        bgView.fitHeightProperty().bind(rootPane.heightProperty());
+        bgView.setPreserveRatio(false);
+
+
+        rootPane.getChildren().add(0, bgView);
+
+
+        Image icon = new Image(Objects.requireNonNull(getClass().getResource("/image/logo/logo.png")).toExternalForm());
+        primaryStage.getIcons().add(icon);
+
+
+        primaryStage.setTitle("MeetGrid Login");
+        primaryStage.setMinWidth(900);
+        primaryStage.setMinHeight(600);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(scene);
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.show();
+        FadeTransition fade = new FadeTransition(Duration.millis(1500), rootPane);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.play();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ meetgrid-client/src/main/java/ru/funduruk/controller/LoginController.java
@@ -0,0 +1,140 @@
+package ru.funduruk.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+
+    @FXML
+    public BorderPane rootPane;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    VBox statusBox;
+
+    @FXML
+    private void handleLogin() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if(username.isEmpty() || password.isEmpty()) {
+            statusLabel.setText("Username and password required");
+            statusBox.setVisible(true);
+            return;
+        }
+
+
+        if(username.equals("test") && password.equals("1234")) {
+            statusLabel.setText("Login successful!");
+            statusBox.setVisible(true);
+        } else {
+            statusLabel.setText("Invalid credentials");
+            statusBox.setVisible(true);
+        }
+    }
+
+    @FXML
+    private void handleRegister() {
+        statusLabel.setText("Register clicked!");
+    }
+
+    @FXML private HBox titleBar;
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+
+
+    @FXML
+    public void initialize() {
+        enableWindowDragging();
+        enableWindowResize();
+    }
+
+    private void enableWindowDragging() {
+        titleBar.setOnMousePressed(event -> {
+            Stage stage = (Stage) titleBar.getScene().getWindow();
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        titleBar.setOnMouseDragged(event -> {
+            Stage stage = (Stage) titleBar.getScene().getWindow();
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+    }
+
+    private boolean isInResizeZone(MouseEvent event) {
+        double mouseX = event.getX();
+        double mouseY = event.getY();
+        double width = rootPane.getWidth();
+        double height = rootPane.getHeight();
+        final int RESIZE_MARGIN = 10;
+
+        return mouseX > width - RESIZE_MARGIN || mouseY > height - RESIZE_MARGIN;
+    }
+
+    private void enableWindowResize() {
+        rootPane.setOnMouseMoved(event -> {
+            Stage stage = (Stage) rootPane.getScene().getWindow();
+            if (isInResizeZone(event)) {
+                rootPane.setCursor(Cursor.SE_RESIZE);
+            } else {
+                rootPane.setCursor(Cursor.DEFAULT);
+            }
+        });
+
+        rootPane.setOnMouseDragged(event -> {
+            Stage stage = (Stage) rootPane.getScene().getWindow();
+
+            if (rootPane.getCursor() == Cursor.SE_RESIZE) {
+                double newWidth = event.getX();
+                double newHeight = event.getY();
+
+
+                if (newWidth < stage.getMinWidth()) newWidth = stage.getMinWidth();
+                if (newHeight < stage.getMinHeight()) newHeight = stage.getMinHeight();
+
+                stage.setWidth(newWidth);
+                stage.setHeight(newHeight);
+            }
+        });
+    }
+
+    @FXML
+    private void close() {
+        ((Stage) titleBar.getScene().getWindow()).close();
+    }
+
+    @FXML
+    private void minimize() {
+        ((Stage) titleBar.getScene().getWindow()).setIconified(true);
+    }
+
+    @FXML
+    private void maximize() {
+        Stage stage = (Stage) titleBar.getScene().getWindow();
+        stage.setMaximized(!stage.isMaximized());
+    }
+
+
+
+}
